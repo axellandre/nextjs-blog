a3fca4fcbd165ac376711ac29e5f9bba05d2780f
Axel <a.landre@proxiad.com>
2023-01-31 14:07:17 +0100

    fix problems

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,15 @@
 module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     // For the best performance and to avoid false positives,
     // be as specific as possible with your content configuration.
   ],
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-  plugins: [require("postcss-import"), require("postcss-preset-env")],
 };

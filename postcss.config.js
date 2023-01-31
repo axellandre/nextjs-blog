a9797bb85f209ea3aa24708f72e69347574d3e1a
Axel <a.landre@proxiad.com>
2023-01-31 12:16:00 +0100

    tuto nextjs

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // For the best performance and to avoid false positives,
+    // be as specific as possible with your content configuration.
+  ],
+};
